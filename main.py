bbff912e5785f0438c8c8a4edf4b9ae4b6c10f55
Onda Reki <77719187+reki204@users.noreply.github.com>
2022-03-26 17:07:31 +0900

    main.pyの一部修正

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, abort
 from linebot import (
-  LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-  InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
-  MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 import os
 
@@ -14,37 +14,35 @@ app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
- 
+
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
- 
-@app.route("/")
-def hello_world():
-  return "Idea×Techの新歓へようこそ"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-  # get X-Line-Signature header value
-  signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-  # get request body as text
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-  # handle webhook body
-  try:
-      handler.handle(body, signature)
-  except InvalidSignatureError:
-      abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-  return 'OK'
- 
+    return 'OK'
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
-  port = int(os.getenv("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
