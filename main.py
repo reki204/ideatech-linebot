67bcc063d2b73f44d7f9710d4ced0489894ad0d4
Onda Reki <77719187+reki204@users.noreply.github.com>
2022-03-26 19:08:29 +0900

    push機能追加

--- main.py
+++ main.py
@@ -35,12 +35,25 @@ def callback():
 
     return 'OK'
 
+# push通知機能
+# def push():
+#     user_id = ""
+#     messages = TextSendMessage(text=f"こんにちは！")
+#     line_bot_api.push_message(user_id, messages=messages)
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    profile = line_bot_api.get_profile(event.source.user_id)
+
+    # ユーザー情報取得
+    user_id = profile.user_id
+    # pushするメッセージを作成
+    messages = TextSendMessage(text=f"こんにちは！")
+    
+    # ユーザーにメッセージを送信する
+    line_bot_api.push_message(user_id, messages=messages)
+    # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
